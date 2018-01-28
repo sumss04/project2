c39a56fb4030cef2cc7ee7196e735ba79d3aae5b
Mohammad Ali <Mohammad Ali@192.168.0.41>
2018-01-27 21:02:45 -0500

    version 0.06

---
+++ Maven_Project2/src/test/java/com/usa/thirdjava/sum/TestEncapsualtionChild.java
@@ -0,0 +1,16 @@
+package com.usa.thirdjava.sum;
+
+public class TestEncapsualtionChild extends TestEncapsualtionParent{
+	public static void main(String[] args) {
+	TestEncapsualtionChild obj=new TestEncapsualtionChild();
+	obj.getA();
+	obj.setA(10);
+	System.out.println(10);
+	
+		
+	}
+	
+
+
+	
+}
---
+++ Maven_Project2/src/test/java/com/usa/thirdjava/sum/TestEncapsualtionParent.java
@@ -0,0 +1,22 @@
+package com.usa.thirdjava.sum;
+
+public class TestEncapsualtionParent {
+	
+	
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	  }
+	
+	
+	
+	}
+	
+	
+
+
